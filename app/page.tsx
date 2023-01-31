bdbbc662b83216ab167948e6378f98adf4363d1f
Ahmad Nawaz <web.develpment.an@gmail.com>
2023-02-01 00:28:31 +0500

    step00_helloworld

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+    {/* <div className={styles.hellodiv}> */}
+    <div className={styles.divhello}>
+      <h1>Hello World! from Ahmad</h1>
+      <h5>My First Nextjs App deploed at Vercel </h5>
+    </div>
+    {/* </div> */}
+    </>
+  )
+}
